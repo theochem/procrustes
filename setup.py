287ca7e248f40af97e786005414a307bde387305
Jonathan La <lajd@mcmaster.ca>
2017-03-21 15:58:28 -0400

    Add setup.py file.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='procrustes',
+    version='0.0',
+    packages=['procrustes', 'procrustes.hungarian', 'procrustes.procrustes', 'procrustes.procrustes.test'],
+    url='',
+    #test_suite='nose.collector',
+    license='MIT',
+    author='Jonathan La, Farnaz Zadeh',
+    author_email='',
+    description='A package for basic procrustes problems'
+)

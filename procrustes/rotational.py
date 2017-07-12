24f0f79c5a086e51652b1353cebbd047cedfa5b9
Farnaz Heidar-Zadeh <farnaz_chem@yahoo.com>
2017-07-12 01:10:26 -0400

    Revisit rotational procrustes code, docs & tests
    1. Clean up latex equations and docstring.
    2. Made variable names consistent and added new attributes.
    3. Add a separate file for testing this module, and moved the tests from
       test_procrustes.py file. The tests were then made more clear and
       concise.

--- procrustes/rotational.py
+++ procrustes/rotational.py
@@ -21,7 +21,9 @@
 #
 # --
 """
+Rotational Orthogonal Procrustes Module.
 """
+
 
 from procrustes.base import Procrustes
 from procrustes.utils import singular_value_decomposition
@@ -32,27 +34,27 @@ class RotationalOrthogonalProcrustes(Procrustes):
     r"""
     Rotational Orthogonal Procrustes Class.
 
-    Given a matrix :math:`A_{m \times n}` and a reference matrix :math:`A^0_{m \times n}`,
-    find the unitary/orthogonla transformation of :math:`A_{m \times n}` that makes it as
-    close as possible to :math:`A^0_{m times n}`. I.e.,
+    Given matrix :math:`\mathbf{A}_{m \times n}` and a reference :math:`\mathbf{B}_{m \times n}`,
+    find the transformation of :math:`\mathbf{A}_{m \times n}` that makes it as close as possible
+    to :math:`\mathbf{B}_{m \times n}`. I.e.,
 
     .. math::
        \underbrace{\min}_{\left\{\mathbf{U} \left| {\mathbf{U}^{-1} = {\mathbf{U}}^\dagger
                                 \atop \left| \mathbf{U} \right| = 1} \right. \right\}}
-          \|\mathbf{A}\mathbf{U} - \mathbf{A}^0\|_{F}^2
+          \|\mathbf{A}\mathbf{U} - \mathbf{B}\|_{F}^2
        &= \underbrace{\min}_{\left\{\mathbf{U} \left| {\mathbf{U}^{-1} = {\mathbf{U}}^\dagger
                                    \atop \left| \mathbf{U} \right| = 1} \right. \right\}}
-          \text{Tr}\left[\left(\mathbf{A}\mathbf{U} - \mathbf{A}^0 \right)^\dagger
-                         \left(\mathbf{A}\mathbf{U} - \mathbf{A}^0 \right)\right] \\
+          \text{Tr}\left[\left(\mathbf{A}\mathbf{U} - \mathbf{B} \right)^\dagger
+                         \left(\mathbf{A}\mathbf{U} - \mathbf{B} \right)\right] \\
        &= \underbrace{\max}_{\left\{\mathbf{U} \left| {\mathbf{U}^{-1} = {\mathbf{U}}^\dagger
                                    \atop \left| \mathbf{U} \right| = 1} \right. \right\}}
-          \text{Tr}\left[\mathbf{U}^\dagger {\mathbf{A}}^\dagger \mathbf{A}^0 \right]
+          \text{Tr}\left[\mathbf{U}^\dagger {\mathbf{A}}^\dagger \mathbf{B} \right]
 
-    The solution of obtained by taking the singular value decomposition (SVD) of the product of
-    the matrices,
+    Here, :math:`\mathbf{U}_{n \times n}` is the permutation matrix. The solution is obtained by
+    taking the singular value decomposition (SVD) of the product of the matrix,
 
     .. math::
-       \mathbf{A}^\dagger \mathbf{A}^0 &= \tilde{\mathbf{U}} \tilde{\mathbf{\Sigma}}
+       \mathbf{A}^\dagger \mathbf{B} &= \tilde{\mathbf{U}} \tilde{\mathbf{\Sigma}}
                                           \tilde{\mathbf{V}}^{\dagger} \\
        \mathbf{U}_{\text{optimum}} &= \tilde{\mathbf{U}} \tilde{\mathbf{S}}
                                       \tilde{\mathbf{V}}^{\dagger}
@@ -81,15 +83,15 @@ class RotationalOrthogonalProcrustes(Procrustes):
     """
 
     def __init__(self, array_a, array_b, translate=False, scale=False):
-        """
+        r"""
         Initialize the class and transfer/scale the arrays followed by computing transformaion.
 
         Parameters
         ----------
         array_a : ndarray
-            The 2d-array :math: `A_{m \times n}` which is going to be transformed.
+            The 2d-array :math:`\mathbf{A}_{m \times n}` which is going to be transformed.
         array_b : ndarray
-            The 2d-array :math: `A^0_{m \times n}` represents the reference matrix.
+            The 2d-array :math:`\mathbf{B}_{m \times n}` represents the reference matrix.
         translate : bool
             If True, both arrays are translated to be centered at origin, default=False.
         scale : bool
@@ -100,18 +102,27 @@ class RotationalOrthogonalProcrustes(Procrustes):
         The Procrustes analysis requires two 2d-arrays with the same number of rows, so the
         array with the smaller number of rows will automatically be padded with zero rows.
         """
-        super(RotationalOrthogonalProcrustes, self).__init__(
-            array_a, array_b, translate, scale)
+        super(self.__class__, self).__init__(array_a, array_b, translate, scale)
 
         # compute transformation
-        self.array_u = self.compute_transformation()
+        self._array_u = self._compute_transformation()
 
         # calculate the single-sided error
-        self.error = self.single_sided_error(self.array_u)
+        self._error = self.single_sided_error(self._array_u)
 
-    def compute_transformation(self):
+    @property
+    def array_u(self):
+        r"""Transformation matrix :math:`\mathbf{U}_{n \times n}`."""
+        return self._array_u
+
+    @property
+    def error(self):
+        """Procrustes error."""
+        return self._error
+
+    def _compute_transformation(self):
         """
-        Return the optimal rotational-orthogonal transformation array :math:`\mathbf{U}`.
+        Compute optimal rotational-orthogonal transformation array.
 
         Returns
         -------
@@ -122,8 +133,8 @@ class RotationalOrthogonalProcrustes(Procrustes):
         product = np.dot(self.array_a.T, self.array_b)
         u, s, v_trans = singular_value_decomposition(product)
 
-        # construct matrix S which is an identity matrix where the smallest
-        # singular value is replaced with sgn(|U*V^t|).
+        # construct S which is an identity matrix with the smallest
+        # singular value replaced by sgn(|U*V^t|).
         array_s = np.eye(self.array_a.shape[1])
         array_s[-1, -1] = np.sign(np.linalg.det(np.dot(u, v_trans)))
 
---
+++ procrustes/test/test_rotational.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+# Procrustes is a collection of interpretive chemical tools for
+# analyzing outputs of the quantum chemistry calculations.
+#
+# Copyright (C) 2017-2018 The Procrustes Development Team
+#
+# This file is part of Procrustes.
+#
+# Procrustes is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# Procrustes is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>
+#
+# --
+
+
+import numpy as np
+from procrustes import RotationalOrthogonalProcrustes
+from numpy.testing import assert_almost_equal
+
+
+def test_rotational_orthogonal_identical():
+    # define an arbitrary array
+    array_a = np.array([[3, 6, 2, 1], [5, 6, 7, 6], [2, 1, 1, 1]])
+    # compute procrustes transformation
+    rot = RotationalOrthogonalProcrustes(array_a, array_a)
+    # check transformation array and error
+    assert_almost_equal(np.dot(rot.array_u, rot.array_u.T), np.eye(4), decimal=8)
+    assert_almost_equal(np.linalg.det(rot.array_u), 1.0, decimal=8)
+    assert_almost_equal(rot.error, 0, decimal=8)
+
+
+def test_rotational_orthogonal_rotation_pad():
+    # define an arbitrary array
+    array_a = np.array([[1, 7], [9, 4]])
+    # define array_b by rotating array_a and pad with zeros
+    theta = np.pi/4
+    rot_array = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
+    array_b = np.dot(array_a, rot_array)
+    array_b = np.concatenate((array_b, np.zeros((2, 10))), axis=1)
+    array_b = np.concatenate((array_b, np.zeros((15, 12))), axis=0)
+    # compute procrustes transformation
+    rot = RotationalOrthogonalProcrustes(array_a, array_b)
+    # check transformation array and error
+    assert_almost_equal(np.dot(rot.array_u, rot.array_u.T), np.eye(2), decimal=8)
+    assert_almost_equal(np.linalg.det(rot.array_u), 1.0, decimal=8)
+    assert_almost_equal(rot.error, 0, decimal=8)
+
+
+def test_rotational_orthogonal_rotation_translate_scale():
+    # define an arbitrary array
+    array_a = np.array([[1., 7., 8.], [4., 6., 8.], [7., 9., 4.], [6., 8., 23.]])
+    # define array_b by scale and translation of array_a and then rotation
+    shift = np.array([[3., 21., 21.], [3., 21., 21.], [3., 21., 21.], [3., 21., 21.]])
+    theta = 44.3 * np.pi / 5.7
+    rot_array = np.array([[np.cos(theta), -np.sin(theta), 0],
+                          [np.sin(theta), np.cos(theta), 0], [0, 0, 1]])
+    array_b = np.dot(477.412 * array_a + shift, rot_array)
+    # compute procrustes transformation
+    rot = RotationalOrthogonalProcrustes(array_a, array_b, translate=True, scale=True)
+    # check transformation array and error
+    assert_almost_equal(np.dot(rot.array_u, rot.array_u.T), np.eye(3), decimal=8)
+    assert_almost_equal(np.linalg.det(rot.array_u), 1.0, decimal=8)
+    assert_almost_equal(rot.error, 0, decimal=8)
+
+
+def test_rotational_orthogonal_rotation_translate_scale_4by3():
+    # define an arbitrary array
+    array_a = np.array([[31.4, 17.5, 18.4], [34.5, 26.5, 28.6],
+                        [17.6, 19.3, 34.6], [46.3, 38.5, 23.3]])
+    # define array_b by scale and translation of array_a and then rotation
+    shift = np.array([[13.3, 21.5, 21.8], [13.3, 21.5, 21.8],
+                      [13.3, 21.5, 21.8], [13.3, 21.5, 21.8]])
+    theta = 4.24 * np.pi / 1.23
+    rot_array = np.array([[np.cos(theta), -np.sin(theta), 0],
+                          [np.sin(theta), np.cos(theta), 0], [0, 0, 1]])
+    array_b = np.dot(12.54 * array_a + shift, rot_array)
+    # Proceed with rotational-orthogonal procrustes analysis
+    rot = RotationalOrthogonalProcrustes(array_a, array_b, translate=True, scale=True)
+    # check transformation array and error
+    assert_almost_equal(np.dot(rot.array_u, rot.array_u.T), np.eye(3), decimal=8)
+    assert_almost_equal(np.linalg.det(rot.array_u), 1.0, decimal=8)
+    assert_almost_equal(rot.error, 0., decimal=8)
+
+
+def test_rotational_orthogonal_zero_array():
+    # define an arbitrary array
+    array_a = np.array([[4.35e-5, 1.52e-5, 8.16e-5], [4.14e-6, 16.41e-5, 18.3e-6],
+                        [17.53e-5, 29.53e-5, 34.56e-5], [26.53e-5, 38.63e-5, 23.36e-5]])
+    # define array_b by scale and translation of array_a and then rotation
+    shift = np.array([[3.25e-6, 21.52e-6, 21.12e-6], [3.25e-6, 21.52e-6, 21.12e-6],
+                      [3.25e-6, 21.52e-6, 21.12e-6], [3.25e-6, 21.52e-6, 21.12e-6]])
+    theta = 1.12525 * np.pi / 5.642
+    rot_array = np.array([[np.cos(theta), -np.sin(theta), 0],
+                          [np.sin(theta), np.cos(theta), 0], [0, 0, 1]])
+    array_b = np.dot(4.12 * array_a + shift, rot_array)
+    # Proceed with rotational-orthogonal procrustes analysis
+    rot = RotationalOrthogonalProcrustes(array_a, array_b, translate=True, scale=True)
+    # check transformation array and error
+    assert_almost_equal(np.dot(rot.array_u, rot.array_u.T), np.eye(3), decimal=8)
+    assert_almost_equal(np.linalg.det(rot.array_u), 1.0, decimal=8)
+    assert_almost_equal(rot.error, 0., decimal=8)

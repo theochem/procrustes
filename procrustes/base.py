9751e941f16f7328f89e3f7d96ec3ea594fcd079
Farnaz Heidar-Zadeh <farnaz_chem@yahoo.com>
2017-07-16 13:53:29 -0400

    Revisit base module
    1. Change A0 to B to be consistent with other module.
    2. Add attributes array_a and array_b as property.
    3. Update docstrings.

--- procrustes/base.py
+++ procrustes/base.py
@@ -43,24 +43,17 @@ class Procrustes(object):
         array_a : ndarray
             The 2d-array :math:`\mathbf{A}_{m \times n}` which is going to be transformed.
         array_b : ndarray
-            The 2d-array :math:`\mathbf{A}^0_{m \times n}` representing the reference.
-        translate : bool, default="False"
+            The 2d-array :math:`\mathbf{B}_{m \times n}` representing the reference.
+        translate : bool, default=False
             If True, both arrays are translated to be centered at origin.
-        scale : bool, default="False"
+        scale : bool, default=False
             If True, both arrays are column normalized to unityalse.
-
-        Notes
-        -----
-        The Procrustes analysis requires two 2d-arrays with the same number of rows, so the
-        array with the smaller number of rows will automatically be padded with zero rows.
         """
         # sanity checks of type and dimension
         if not isinstance(array_a, np.ndarray) or not isinstance(array_b, np.ndarray):
-            raise ValueError(
-                'Arguments array_a and array_b should be of numpy.ndarray type.')
+            raise ValueError('Arguments array_a and array_b should be of numpy.ndarray type.')
         if array_a.ndim != 2 or array_b.ndim != 2:
-            raise ValueError(
-                'Arguments array_a and array_b should be 2D arrays.')
+            raise ValueError('Arguments array_a and array_b should be 2D arrays.')
 
         # remove already padded zero rows and columns (important for translation)
         array_a = hide_zero_padding(array_a)
@@ -75,32 +68,40 @@ class Procrustes(object):
             array_b, self.scale_b = scale_array(array_b)
 
         # make sure the arrays have the same number of rows
-        # if not, pad the one with smaller number of rows with zero.
         if array_a.shape[0] != array_b.shape[0]:
             array_a, array_b = zero_padding(array_a, array_b, mode='row')
 
-        self.array_a = array_a
-        self.array_b = array_b
+        self._array_a = array_a
+        self._array_b = array_b
+
+    @property
+    def array_a(self):
+        r"""The 2d-array :math:`\mathbf{A}`."""
+        return self._array_a
+
+    @property
+    def array_b(self):
+        r"""The 2d-array :math:`\mathbf{B}`."""
+        return self._array_b
 
     def single_sided_error(self, array_u):
         r"""
         Return the single-sided procrustes error.
 
          .. math::
-            \text{Tr}\left[\left(\mathbf{AU} - \mathbf{A}^0\right)^\dagger
-                           \left(\mathbf{AU} - \mathbf{A}^0\right)\right]
+            \text{Tr}\left[\left(\mathbf{AU} - \mathbf{B}\right)^\dagger
+                           \left(\mathbf{AU} - \mathbf{B}\right)\right]
 
         Parameters
         ----------
         array_u : ndarray
-           A 2D array representing the transformation :math:`\mathbf{U}`.
+           The 2d-array representing the transformation :math:`\mathbf{U}`.
 
         Returns
         -------
         error : float
             The single-sided error.
         """
-
         au = np.dot(self.array_a, array_u)
         error = np.trace(np.dot((au - self.array_b).T, au - self.array_b))
         return error
@@ -110,23 +111,22 @@ class Procrustes(object):
         Return the double-sided procrustes error.
 
          .. math::
-            \text{Tr}\left[\left(\mathbf{U_1}^\dagger \mathbf{AU_2} - \mathbf{A}^0\right)^\dagger
-                           \left(\mathbf{U_1}^\dagger \mathbf{AU_2} - \mathbf{A}^0\right)\right]
+            \text{Tr}\left[
+                 \left(\mathbf{U}_1^\dagger \mathbf{A}\mathbf{U}_2 - \mathbf{B}\right)^\dagger
+                 \left(\mathbf{U}_1^\dagger \mathbf{A}\mathbf{U}_2 - \mathbf{B}\right)\right]
 
         Parameters
         ----------
         array_u1 : ndarray
-           A 2D array representing the 1st transformation :math:`\mathbf{U_1}`.
+           The 2d-array representing the left-hand-side transformation :math:`\mathbf{U_1}`.
         array_u2 : ndarray
-           A 2D array representing the 2nd transformation :math:`\mathbf{U_2}`.
+           The 2d-array representing the right-hand-side transformation :math:`\mathbf{U_2}`.
 
         Returns
         -------
         error : float
             The double-sided error.
         """
-
         u1au2 = np.dot(np.dot(array_u1.T, self.array_a), array_u2)
-        error = np.trace(
-            np.dot((u1au2 - self.array_b).T, u1au2 - self.array_b))
+        error = np.trace(np.dot((u1au2 - self.array_b).T, u1au2 - self.array_b))
         return error

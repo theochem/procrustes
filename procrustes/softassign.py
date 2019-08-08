c214b7967911ee561a6eb97e7782ec75035257ff
Fanwang Meng <fwmeng88@gmail.com>
2019-08-08 15:51:50 -0400

    Fix and reformat softassign
    1. Fix the delta values in relaxation and Sinkhorn steps
    2. Add beta_0 and beta_f default values
    3. Add Mai_guess and return_guess options
    4. Fix docstrings

--- procrustes/softassign.py
+++ procrustes/softassign.py
@@ -22,6 +22,8 @@
 # --
 """The Softassign Procrustes Module."""
 
+import warnings
+
 import numpy as np
 
 from procrustes.permutation import permutation
@@ -36,8 +38,9 @@ __all__ = [
 def softassign(A, B, remove_zero_col=True, remove_zero_row=True,
                pad_mode='row-col', translate=False, scale=False,
                check_finite=True, iteration_r=500, iteration_s=500,
-               beta_r=1.075, tol=1.0e-8, epsilon_gamma=0.01,
-               idx_stop=10, beta_0=None, beta_f=None):
+               beta_r=1.075, tol_r=1.0e-8, tol_s=1.0e-8,
+               epsilon_gamma=0.01, idx_stop=10, beta_0=1.e-5,
+               beta_f=1.e4, Mai_guess=None, return_guess=False):
     r"""
     Parameters
     ----------
@@ -48,30 +51,30 @@ def softassign(A, B, remove_zero_col=True, remove_zero_row=True,
         The 2d-array :math:`\mathbf{B}_{m \times n}` representing
         the reference.
     remove_zero_col : bool, optional
-        If True, the zero columns on the right side will be removed.
+        If True, the zero columns on the right side will be removed
         Default=True.
     remove_zero_row : bool, optional
         If True, the zero rows on the top will be removed.
         Default=True.
     pad_mode : str, optional
-      Zero padding mode when the sizes of two arrays differ. Default='row-col'.
-      'row': The array with fewer rows is padded with zero rows so that both have the same
+      Zero padding mode when the sizes of two arrays differ. Defaul
+      'row': The array with fewer rows is padded with zero rows so
            number of rows.
-      'col': The array with fewer columns is padded with zero columns so that both have the
+      'col': The array with fewer columns is padded with zero colum
            same number of columns.
-      'row-col': The array with fewer rows is padded with zero rows, and the array with fewer
-           columns is padded with zero columns, so that both have the same dimensions.
+      'row-col': The array with fewer rows is padded with zero rows
+           columns is padded with zero columns, so that both have t
            This does not necessarily result in square arrays.
-      'square': The arrays are padded with zero rows and zero columns so that they are both
-           squared arrays. The dimension of square array is specified based on the highest
+      'square': The arrays are padded with zero rows and zero colum
+           squared arrays. The dimension of square array is specifi
            dimension, i.e. :math:`\text{max}(n_a, m_a, n_b, m_b)`.'
     translate : bool, optional
-        If True, both arrays are translated to be centered at origin.
+        If True, both arrays are translated to be centered at origi
         Default=False.
     scale : bool, optional
-        If True, both arrays are column normalized to unity. Default=False.
+        If True, both arrays are column normalized to unity. Defaul
     check_finite : bool, optional
-        If true, convert the input to an array, checking for NaNs or Infs.
+        If true, convert the input to an array, checking for NaNs o
         Default=True.
     iteration_r : int, optional
         Number of iterations in relaxation step. Default=500.
@@ -79,17 +82,25 @@ def softassign(A, B, remove_zero_col=True, remove_zero_row=True,
         Number of iterations in Sinkhorn step. Default=500.
     beta_r : float, optional
         Annealing rate which should greater than 1. Default=1.075.
-    tol : float, optional
-        The tolerance value used for relaxation and softassign. Default=1.e-8.
+    tol_r : float, optional
+        The tolerance value used for relaxation. Default=1.e-8.
+    tol_s : float, optional
+        The tolerance value used for sinkhorn. Default=1.e-8.
     epsilon_gamma : float, optional
-        Small quantity which is required to compute gamma. Default=0.01.
+        Small quantity which is required to compute gamma. Default=
     idx_stop : int, optional
-        Number of running steps after the calculation converges in the
+        Number of running steps after the calculation converges in
         relaxation step. Default=10.
     beta_0 : float, optional
-        Initial inverse temperature. Default=None.
+        Initial inverse temperature. Default=1.e-5.
     beta_f : float, optional
-        Final inverse temperatue. Default=None.
+        Final inverse temperatue. Default=1.e4.
+    Mai_guess : numpy.array, optional
+        Initial guess for determinstic annealing. If None, the function
+        will use a random guess. Default=None.
+    return_guess : bool, optional
+        Whether to return the :math:`\mathbf{M}_{ai}` from the last step
+        if the calculations failed. Default=False.
 
     Returns
     -------
@@ -104,16 +115,14 @@ def softassign(A, B, remove_zero_col=True, remove_zero_row=True,
 
     Notes
     -----
-    Quadratic assignment problem (QAP) has played a very special but
-    fundamental role in combinatorial optimization problems. The problem can
-    be defined as a optimization problem to minimize the cost to assign a set
-    of facilities to a set of locations. The cost is a function of the flow
-    between the facilities and the geographical distances among various
+    Quadratic assignment problem (QAP) has played a very special bu
+    fundamental role in combinatorial optimization problems. The pr
+    be defined as a optimization problem to minimize the cost to as
+    of facilities to a set of locations. The cost is a function of
+    between the facilities and the geographical distances among var
     facilities.
-
-    The objective function (also named loss function in machine learning) is
+    The objective function (also named loss function in machine lea
     defined as [1]_
-
     .. math::
         E_{qap}(M, \mu, \nu) =
             - \frac{1}{2}\Sigma_{aibj}C_{ai;bj}M_{ai}M_{bj}
@@ -121,22 +130,21 @@ def softassign(A, B, remove_zero_col=True, remove_zero_row=True,
             + \Sigma_i {\nu}_i (\Sigma_i M_{ai} -1)
             - \frac{\gamma}{2}\Sigma_{ai} {M_{ai}}^2
             + \frac{1}{\beta} \Sigma_{ai} M_{ai}\log{M_{ai}}
-
     where :math:`C_{ai,bj}` is the benefit matrix, :math:`M` is the
     desired :math:`N \times N` permutation matrix. :math:`E` is the
-    energy function which comes along with a self-amplification term with
-    `\gamma`, two Lagrange parameters :math:`\mu` and :math:`\nu` for
-    constrained optimization and :math:`M_{ai} \log{M_{ai}}` servers as a
-    barrier function which ensures positivity of :math:`M_{ai}`. The
+    energy function which comes along with a self-amplification ter
+    `\gamma`, two Lagrange parameters :math:`\mu` and :math:`\nu` f
+    constrained optimization and :math:`M_{ai} \log{M_{ai}}` server
+    barrier function which ensures positivity of :math:`M_{ai}`. Th
     inverse temperature :math:`\beta` is a deterministic annealing
-    control parameter. More detailed information about the algorithm can be
+    control parameter. More detailed information about the algorith
     referred to Rangarajan's paper.
 
     References
     ----------
-    .. [1] Rangarajan, Anand and Yuille, Alan L and Gold, Steven and
-       Mjolsness, Eric, "A convergence proof for the softassign quadratic
-       assignment algorithm" Advances in Neural Information Processing
+    .. [1] Rangarajan, Anand and Yuille, Alan L and Gold, Steven an
+       Mjolsness, Eric, "A convergence proof for the softassign qua
+       assignment algorithm" Advances in Neural Information Process
        Systems, page 620-626, 1997.
 
     Examples
@@ -150,8 +158,8 @@ def softassign(A, B, remove_zero_col=True, remove_zero_row=True,
         # define array_b by permuting array_a
     >>> array_b = np.dot(perm.T, np.dot(array_a, perm))
     >>> new_a, new_b, M_ai, e_opt = softassign(array_a, array_b,
-                                               remove_zero_col=False,
-                                               remove_zero_row=False)
+                                               remove_zero_col=Fals
+                                               remove_zero_row=Fals
     >>> M_ai # the permutation matrix
     array([[0., 0., 1., 0.],
            [1., 0., 0., 0.],
@@ -159,7 +167,6 @@ def softassign(A, B, remove_zero_col=True, remove_zero_row=True,
            [0., 1., 0., 0.]])
     >>> e_opt # the error
     0.0
-
     """
     # Check beta_r
     if beta_r <= 1:
@@ -175,21 +182,28 @@ def softassign(A, B, remove_zero_col=True, remove_zero_row=True,
     C_tensor = C.reshape(N, N, N, N)
     # Compute the beta_0
     gamma = _compute_gamma(C, N, epsilon_gamma)
-    if beta_0 == None:
-        beta_0 = 1.e-5 * np.sqrt(N * N)
-    if beta_f == None:
-        beta_f = 1.e4 * np.sqrt(N * N)
     # Initialization of M_ai
-    M_ai = 1 / N + np.abs(np.random.rand(N, N))
+    # check shape of Mai_guess
+    if Mai_guess is not None:
+        if Mai_guess.shape[0] == N and Mai_guess.shape[1] == N:
+            M_relax_old = Mai_guess
+        else:
+            warning_info = "The shape of Mai_guess does not match (" \
+                           + str(N) + "," + str(N) + "). " \
+                           + "Use random initial guess instead."
+            warnings.warn(warning_info)
+            M_relax_old = 1 / N + np.random.rand(N, N)
+    else:
+        M_relax_old = 1 / N + np.random.rand(N, N)
     beta = beta_0
-    M_relax_old = M_ai
     step_r = 0
     # step to control when to stop the calculation
     idx = 0
+    delta_M_relax = np.inf
     # Deterministic annealing
     while beta < beta_f:
-        delta_relax = (M_relax_old - 0) / N
-        while np.amax(np.abs(delta_relax)) > tol and step_r < iteration_r:
+        # relaxation
+        while np.amax(np.abs(delta_M_relax)) > tol_r and step_r < iteration_r:
             step_r += 1
             if step_r == iteration_r:
                 print('Maximum iteration in relaxation stage reached!')
@@ -197,41 +211,70 @@ def softassign(A, B, remove_zero_col=True, remove_zero_row=True,
             Q = np.einsum('aibj,bj->ai', C_tensor, M_relax_old)
             Q += gamma * M_relax_old
             # soft_assign
-            M_relax_new = np.exp(beta * Q)
-            # Sinkhorn
-            M_sink_old = M_relax_new
+            M_soft = np.exp(beta * Q)
+            # Sinkhorn initial value
+            M_sink_old = M_soft
             # step_s for Shinkhorn balancing
             step_s = 0
             # delta_M_sink = (M_relax_new - M_relax_old)/N
-            delta_M_sink = (M_sink_old - 0) / N
-            while np.amax(np.abs(delta_M_sink)) > tol and step_s < iteration_s:
+            delta_M_sink = np.inf
+            # while np.amax(np.abs(delta_M_sink)) > tol and step_s < iteration_s:
+            while np.amax(np.abs(delta_M_sink)) > tol_s and step_s < iteration_s:
                 step_s += 1
                 # Row normalization
                 M_sink_new = M_sink_old / M_sink_old.sum(axis=1, keepdims=1)
                 # Column normalization
                 M_sink_new = M_sink_new / M_sink_new.sum(axis=0, keepdims=1)
                 # Compute the delata_M_sink
-                delta_M_sink = (M_sink_new - M_sink_old) / N
+                delta_M_sink = M_sink_new - M_sink_old
                 # Update M_sink_old
                 M_sink_old = M_sink_new
-            # use the result of Sinkhorn to update M_ai for
+                # tol_s = max(np.amax(np.abs(delta_M_sink))/20, tol_s)
+            # use the result of Sinkhorn to update M_ai for relaxation
             M_relax_new = M_sink_new
-            # Compute the delta_relax
-            delta_relax = (M_relax_new - M_relax_old) / N
+            # Compute the delta_M_relax
+            delta_M_relax = M_relax_new - M_relax_old
+            # tol_r = max(np.amax(np.abs(delta_M_relax))/20, tol_r)
             # Update the M_relax
             M_relax_old = M_relax_new
 
         beta = beta_r * beta
-        if np.amax(np.abs(delta_relax)) < tol:
-            idx += 1
-            if idx == idx_stop:
-                break
 
-    # Perform clean-up heuristic
-    _, _, M_ai, _ = permutation(np.eye(M_relax_new.shape[0]), M_relax_new)
-    # Compute error
-    e_opt = error(A, B, M_ai, M_ai)
-    return A, B, M_ai, e_opt
+        if np.isnan(M_relax_new).any() or np.isinf(M_relax_new).any():
+            break
+        else:
+            # keep a temporary value for the case of nan
+            M_tmp = M_relax_new
+
+        # if np.amax(np.abs(delta_M_relax)) < tol_r:
+        #     idx += 1
+        #     if idx == idx_stop:
+        #         break
+    #
+    if return_guess:
+        try:
+            _, _, M_ai, _ = permutation(np.eye(M_relax_new.shape[0]), M_relax_new)
+            final_guess = M_relax_new
+        except ValueError:
+            print("M_ai cannot contain infs. Please try to decrease annealing rate (beta_r) or "
+                  "increase the temperature (beta_f).")
+            _, _, M_ai, _ = permutation(np.eye(M_tmp.shape[0]), M_tmp)
+            final_guess = M_tmp
+        # Compute error
+        e_opt = error(A, B, M_ai, M_ai)
+        return A, B, M_ai, e_opt, final_guess
+    else:
+        try:
+            _, _, M_ai, _ = permutation(np.eye(M_relax_new.shape[0]), M_relax_new)
+        except ValueError:
+            print("M_ai cannot contain infs. Please try to decrease annealing rate (beta_r) or "
+                  "increase the temperature (beta_f).")
+            _, _, M_ai, _ = permutation(np.eye(M_tmp.shape[0]), M_tmp)
+
+        # Compute error
+        e_opt = error(A, B, M_ai, M_ai)
+        print(e_opt)
+        return A, B, M_ai, e_opt
 
 
 def _compute_gamma(C, N, epsilon_gamma):

8dd010c0043c1d54f182a91d89d8996afec6f93a
Fanwang Meng <fwmeng88@gmail.com>
2020-02-02 16:26:49 -0500

    Refactor test_symmetric
    1. Add docstrings
    2. Fix import order

--- procrustes/test/test_symmetric.py
+++ procrustes/test/test_symmetric.py
@@ -20,14 +20,15 @@
 # along with this program; if not, see <http://www.gnu.org/licenses/>
 #
 # --
-
+"""Testings for symmetric Procrustes module."""
 
 import numpy as np
-from procrustes import symmetric
 from numpy.testing import assert_almost_equal
+from procrustes import symmetric
 
 
 def test_symmetric_transformed():
+    r"""Test symmetric without translation and scaling."""
     # define arbitrary array and symmetric transformation
     array_a = np.array([[1, 2, 4, 5], [5, 7, 3, 3], [1, 5, 1, 9], [1, 5, 2, 7], [5, 7, 9, 0]])
     sym_array = np.dot(np.array([[1, 7, 4, 9]]).T, np.array([[1, 7, 4, 9]]))
@@ -36,7 +37,7 @@ def test_symmetric_transformed():
     array_b = np.concatenate((array_b, np.zeros((5, 2))), axis=1)
     array_b = np.concatenate((array_b, np.zeros((8, 6))), axis=0)
     # compute procrustes transformation
-    new_a, new_b, array_x, e_opt = symmetric(array_a, array_b, translate=False, scale=False)
+    _, _, array_x, e_opt = symmetric(array_a, array_b, translate=False, scale=False)
     # check transformation is symmetric & error is zero
     assert_almost_equal(array_x, array_x.T, decimal=6)
     assert_almost_equal(array_x, sym_array, decimal=6)
@@ -44,6 +45,7 @@ def test_symmetric_transformed():
 
 
 def test_symmetric_scaled_shifted_tranformed():
+    r"""Test symmetric with translation and scaling."""
     # define an arbitrary array_a, translation matrix & symmetric matrix
     array_a = np.array([[5, 2, 8], [2, 2, 3], [1, 5, 6], [7, 3, 2]], dtype=float)
     shift = np.array([[9., 4., 3.], [9., 4., 3.], [9., 4., 3.], [9., 4., 3.]])
@@ -52,7 +54,7 @@ def test_symmetric_scaled_shifted_tranformed():
     array_b = 614.5 * array_a + shift
     array_b = np.dot(array_b, sym_array)
     # compute procrustes transformation
-    new_a, new_b, array_x, e_opt = symmetric(
+    _, _, array_x, e_opt = symmetric(
         array_a, array_b, translate=True, scale=True)
     # check transformation is symmetric & error is zero
     assert_almost_equal(array_x, array_x.T, decimal=6)
@@ -60,6 +62,7 @@ def test_symmetric_scaled_shifted_tranformed():
 
 
 def test_symmetric_scaled_shifted_tranformed_4by3():
+    r"""Test symmetric by 4by3 array with translation and scaling."""
     # define an arbitrary array_a, translation matrix & symmetric matrix
     array_a = np.array([[245.0, 122.4, 538.5], [122.5, 252.2, 352.2],
                         [152.5, 515.2, 126.5], [357.5, 312.5, 225.5]])
@@ -70,7 +73,7 @@ def test_symmetric_scaled_shifted_tranformed_4by3():
     array_b = 312.5 * array_a + shift
     array_b = np.dot(array_b, sym_array)
     # compute procrustes transformation
-    new_a, new_b, array_x, e_opt = symmetric(
+    _, _, array_x, e_opt = symmetric(
         array_a, array_b, translate=True, scale=True)
     # check transformation is symmetric & error is zero
     assert_almost_equal(array_x, array_x.T, decimal=6)
@@ -78,6 +81,7 @@ def test_symmetric_scaled_shifted_tranformed_4by3():
 
 
 def test_symmetric():
+    r"""Test symmetric by arrays with small values."""
     # define an arbitrary array_a, translation matrix & symmetric matrix
     array_a = np.array([[5.52e-5, 2.15e-5, 8.12e-5], [2.14e-5, 2.22e-5, 3.14e-5],
                         [1.11e-5, 5.94e-5, 6.58e-5], [7.15e-5, 3.62e-5, 2.24e-5]])
@@ -88,7 +92,7 @@ def test_symmetric():
     array_b = 6.61e-4 * array_a + shift
     array_b = np.dot(array_b, sym_array)
     # compute procrustes transformation
-    new_a, new_b, array_x, e_opt = symmetric(
+    _, _, array_x, e_opt = symmetric(
         array_a, array_b, translate=True, scale=True)
     # check transformation is symmetric & error is zero
     assert_almost_equal(array_x, array_x.T, decimal=6)
@@ -96,12 +100,13 @@ def test_symmetric():
 
 
 def test_not_full_rank_case():
+    r"""Test symmetric with not full rank case."""
     # Define a random matrix and symmetric matrix
     array_a = np.array([[10, 83], [52, 58], [58, 44]])
     sym_array = np.array([[0.38895636, 0.30523869], [0.30523869, 0.30856369]])
     array_b = np.dot(array_a, sym_array)
     # compute procrustes transformation
-    new_a, new_b, array_x, e_opt = symmetric(array_a, array_b)
+    _, _, array_x, e_opt = symmetric(array_a, array_b)
     # check transformation is symmetric & error is zero
     assert_almost_equal(array_x, array_x.T, decimal=6)
     assert_almost_equal(e_opt, 0, decimal=6)

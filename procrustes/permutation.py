e7868860b8ebbba5b4185570070c9fb36772f1b9
Fanwang Meng <fwmeng88@gmail.com>
2020-02-17 23:42:10 -0500

    Fix unexpected unindent error

--- procrustes/permutation.py
+++ procrustes/permutation.py
@@ -51,17 +51,17 @@ def permutation(array_a, array_b, remove_zero_col=True, remove_zero_row=True,
     remove_zero_row : bool, optional
         If True, the zero rows on the top will be removed. Default= True.
     pad_mode : str, optional
-      Zero padding mode when the sizes of two arrays differ. Default="row-col".
-      "row": The array with fewer rows is padded with zero rows so that both have the same
-           number of rows.
-      "col": The array with fewer columns is padded with zero columns so that both have the
-           same number of columns.
-      "row-col": The array with fewer rows is padded with zero rows, and the array with fewer
-           columns is padded with zero columns, so that both have the same dimensions.
-           This does not necessarily result in square arrays.
-      "square": The arrays are padded with zero rows and zero columns so that they are both
-           squared arrays. The dimension of square array is specified based on the highest
-           dimension, i.e. :math:`\text{max}(n_a, m_a, n_b, m_b)`."
+        Zero padding mode when the sizes of two arrays differ. Default="row-col".
+        "row": The array with fewer rows is padded with zero rows so that both have the same number
+        of rows.
+        "col": The array with fewer columns is padded with zero columns so that both have the
+        same number of columns.
+        "row-col": The array with fewer rows is padded with zero rows, and the array with fewer
+        columns is padded with zero columns, so that both have the same dimensions.
+        This does not necessarily result in square arrays.
+        "square": The arrays are padded with zero rows and zero columns so that they are both
+        squared arrays. The dimension of square array is specified based on the highest dimension,
+        i.e. :math:`\text{max}(n_a, m_a, n_b, m_b)`."
     translate : bool, optional
         If True, both arrays are translated to be centered at origin. Default=False.
     scale : bool, optional
@@ -156,17 +156,17 @@ def permutation_2sided(array_a, array_b, transform_mode="single_undirected",
     remove_zero_row : bool, optional
         If True, the zero rows on the top will be removed. Default= True.
     pad_mode : str, optional
-      Zero padding mode when the sizes of two arrays differ. Default="row-col".
-      "row": The array with fewer rows is padded with zero rows so that both have the same
-           number of rows.
-      "col": The array with fewer columns is padded with zero columns so that both have the
-           same number of columns.
-      "row-col": The array with fewer rows is padded with zero rows, and the array with fewer
-           columns is padded with zero columns, so that both have the same dimensions.
-           This does not necessarily result in square arrays.
-      "square": The arrays are padded with zero rows and zero columns so that they are both
-           squared arrays. The dimension of square array is specified based on the highest
-           dimension, i.e. :math:`\text{max}(n_a, m_a, n_b, m_b)`."
+        Zero padding mode when the sizes of two arrays differ. Default="row-col".
+        "row": The array with fewer rows is padded with zero rows so that both have the same number
+        of rows.
+        "col": The array with fewer columns is padded with zero columns so that both have the
+        same number of columns.
+        "row-col": The array with fewer rows is padded with zero rows, and the array with fewer
+        columns is padded with zero columns, so that both have the same dimensions.
+        This does not necessarily result in square arrays.
+        "square": The arrays are padded with zero rows and zero columns so that they are both
+        squared arrays. The dimension of square array is specified based on the highest dimension,
+        i.e. :math:`\text{max}(n_a, m_a, n_b, m_b)`."
     translate : bool, optional
         If True, both arrays are translated to be centered at origin. Default=False.
     scale : bool, optional
@@ -689,17 +689,17 @@ def permutation_2sided_explicit(array_a, array_b,
     remove_zero_row : bool, optional
         If True, the zero rows on the top will be removed. Default= True.
     pad_mode : str, optional
-      Zero padding mode when the sizes of two arrays differ. Default="row-col".
-      "row": The array with fewer rows is padded with zero rows so that both have the same
-           number of rows.
-      "col": The array with fewer columns is padded with zero columns so that both have the
-           same number of columns.
-      "row-col": The array with fewer rows is padded with zero rows, and the array with fewer
-           columns is padded with zero columns, so that both have the same dimensions.
-           This does not necessarily result in square arrays.
-      "square": The arrays are padded with zero rows and zero columns so that they are both
-           squared arrays. The dimension of square array is specified based on the highest
-           dimension, i.e. :math:`\text{max}(n_a, m_a, n_b, m_b)`."
+        Zero padding mode when the sizes of two arrays differ. Default="row-col".
+        "row": The array with fewer rows is padded with zero rows so that both have the same number
+        of rows.
+        "col": The array with fewer columns is padded with zero columns so that both have the
+        same number of columns.
+        "row-col": The array with fewer rows is padded with zero rows, and the array with fewer
+        columns is padded with zero columns, so that both have the same dimensions.
+        This does not necessarily result in square arrays.
+        "square": The arrays are padded with zero rows and zero columns so that they are both
+        squared arrays. The dimension of square array is specified based on the highest dimension,
+        i.e. :math:`\text{max}(n_a, m_a, n_b, m_b)`."
     translate : bool, optional
         If True, both arrays are translated to be centered at origin. Default=False.
     scale : bool, optional

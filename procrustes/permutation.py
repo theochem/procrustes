159bea3648333a6cb5bdd0e7d1e55aec4362a8ed
Ali-Tehrani <alirezatehrani24@gmail.com>
2020-06-21 15:19:21 -0400

    Fix layout of padding attribute in permutations.py

--- procrustes/permutation.py
+++ procrustes/permutation.py
@@ -51,17 +51,22 @@ def permutation(array_a, array_b, remove_zero_col=True, remove_zero_row=True,
     remove_zero_row : bool, optional
         If True, the zero rows on the top will be removed. Default= True.
     pad_mode : str, optional
-        Zero padding mode when the sizes of two arrays differ. Default="row-col".
-        "row": The array with fewer rows is padded with zero rows so that both have the same number
-        of rows.
-        "col": The array with fewer columns is padded with zero columns so that both have the
-        same number of columns.
-        "row-col": The array with fewer rows is padded with zero rows, and the array with fewer
-        columns is padded with zero columns, so that both have the same dimensions.
-        This does not necessarily result in square arrays.
-        "square": The arrays are padded with zero rows and zero columns so that they are both
-        squared arrays. The dimension of square array is specified based on the highest dimension,
-        i.e. :math:`\text{max}(n_a, m_a, n_b, m_b)`."
+        Specifying how to pad the arrays, listed below. Default="row-col".
+
+            - "row"
+                The array with fewer rows is padded with zero rows so that both have the same
+                number of rows.
+            - "col"
+                The array with fewer columns is padded with zero columns so that both have the
+                same number of columns.
+            - "row-col"
+                The array with fewer rows is padded with zero rows, and the array with fewer
+                columns is padded with zero columns, so that both have the same dimensions.
+                This does not necessarily result in square arrays.
+            - "square"
+                The arrays are padded with zero rows and zero columns so that they are both
+                squared arrays. The dimension of square array is specified based on the highest
+                dimension, i.e. :math:`\text{max}(n_a, m_a, n_b, m_b)`.
     translate : bool, optional
         If True, both arrays are translated to be centered at origin, ie columns of the arrays
         will have mean zero.
@@ -703,17 +708,22 @@ def permutation_2sided_explicit(array_a, array_b,
     remove_zero_row : bool, optional
         If True, near zero rows (less than 1e-8) on the bottom will be removed. Default= True.
     pad_mode : str, optional
-        Zero padding mode when the sizes of two arrays differ. Default="row-col".
-        "row": The array with fewer rows is padded with zero rows so that both have the same number
-        of rows.
-        "col": The array with fewer columns is padded with zero columns so that both have the
-        same number of columns.
-        "row-col": The array with fewer rows is padded with zero rows, and the array with fewer
-        columns is padded with zero columns, so that both have the same dimensions.
-        This does not necessarily result in square arrays.
-        "square": The arrays are padded with zero rows and zero columns so that they are both
-        squared arrays. The dimension of square array is specified based on the highest dimension,
-        i.e. :math:`\text{max}(n_a, m_a, n_b, m_b)`."
+        Specifying how to pad the arrays, listed below. Default="row-col".
+
+            - "row"
+                The array with fewer rows is padded with zero rows so that both have the same
+                number of rows.
+            - "col"
+                The array with fewer columns is padded with zero columns so that both have the
+                same number of columns.
+            - "row-col"
+                The array with fewer rows is padded with zero rows, and the array with fewer
+                columns is padded with zero columns, so that both have the same dimensions.
+                This does not necessarily result in square arrays.
+            - "square"
+                The arrays are padded with zero rows and zero columns so that they are both
+                squared arrays. The dimension of square array is specified based on the highest
+                dimension, i.e. :math:`\text{max}(n_a, m_a, n_b, m_b)`.
     translate : bool, optional
         If True, both arrays are translated to be centered at origin. Default=False.
     scale : bool, optional

e5355feae1f38ef7f2ad348657ba47dbe01ac613
Fanwang Meng <fwmeng88@gmail.com>
2021-02-19 14:16:44 -0500

    Rename error in ProcrustesResult for generic

--- procrustes/generic.py
+++ procrustes/generic.py
@@ -93,7 +93,7 @@ def generic(array_a, array_b,
         The transformed ndarray array_b.
     array_u : ndarray
         The optimum symmetric transformation array.
-    e_opt : float
+    error : float
         One-sided Procrustes error.
 
     Notes
@@ -124,4 +124,4 @@ def generic(array_a, array_b,
     a_inv = np.linalg.pinv(np.dot(new_a.T, new_a))
     array_x = np.linalg.multi_dot([a_inv, new_a.T, new_b])
     e_opt = compute_error(new_a, new_b, array_x)
-    return ProcrustesResult(new_a=new_a, new_b=new_b, array_u=array_x, e_opt=e_opt)
+    return ProcrustesResult(new_a=new_a, new_b=new_b, array_u=array_x, error=e_opt)

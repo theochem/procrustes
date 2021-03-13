a9e6df04577389b2c7c94161a1334c090d490c4f
Farnaz Heidar-Zadeh <farnaz_chem@yahoo.com>
2021-03-13 14:10:41 -0800

    Update docstrings, comments, & tests of symmetric

--- procrustes/symmetric.py
+++ procrustes/symmetric.py
@@ -50,8 +50,9 @@ def symmetric(
                         \right. \right\}} \|\mathbf{A} \mathbf{X} - \mathbf{B}\|_{F}^2
 
     This Procrustes method requires the :math:`\mathbf{A}` and :math:`\mathbf{B}` matrices to
-    have the same shape, which is gauranteed with the default ``pad`` argument for any given
-    :math:`\mathbf{A}` and :math:`\mathbf{B}` matrices. In preparing the :math:`\mathbf{A}` and
+    have the same shape with :math:`m \geqslant n`, which is guaranteed with the default ``pad``
+    argument for any given :math:`\mathbf{A}` and :math:`\mathbf{B}` matrices.
+    In preparing the :math:`\mathbf{A}` and
     :math:`\mathbf{B}` matrices, the (optional) order of operations is: **1)** unpad zero
     rows/columns, **2)** translate the matrices to the origin, **3)** weight entries of
     :math:`\mathbf{A}`, **4)** scale the matrices to have unit norm, **5)** pad matrices with zero
@@ -106,20 +107,19 @@ def symmetric(
     Considering the singular value decomposition of :math:`\mathbf{A}`,
 
     .. math::
-       \mathbf{A}_{m \times n} = \mathbf{U}_{m \times m} \begin{bmatrix}
-                                 \mathbf{\Sigma}_{m \times m} \\
-                                 \mathbf{0}_{m \times (n - m)} \end{bmatrix}
+       \mathbf{A}_{m \times n} = \mathbf{U}_{m \times m}
+                                 \mathbf{\Sigma}_{m \times n}
                                  \mathbf{V}_{n \times n}^\dagger
 
-    where :math:`\mathbf{\Sigma}_{n \times n}` is a square diagonal matrix with nonnegative elements
-    denoted by :math:`\sigma_i` listed in decreasing order, define
+    where :math:`\mathbf{\Sigma}_{m \times n}` is a rectangular diagonal matrix with non-negative
+    singular values :math:`\sigma_i = [\mathbf{\Sigma}]_{ii}` listed in descending order, define
 
     .. math::
        \mathbf{C}_{m \times n} = \mathbf{U}_{m \times m}^\dagger
                                  \mathbf{B}_{m \times n} \mathbf{V}_{n \times n}
 
     with elements denoted by :math:`c_{ij}`.
-    Then we compute the upper triangle of the symmetric matrix :math:`\mathbf{Y}_{n \times n}` with
+    Then we compute the symmetric matrix :math:`\mathbf{Y}_{n \times n}` with
 
     .. math::
        [\mathbf{Y}]_{ij} = \begin{cases}
@@ -162,11 +162,11 @@ def symmetric(
     if (new_a.shape[0] < new_a.shape[1]) or (new_b.shape[0] < new_b.shape[1]):
         new_a, new_b = _zero_padding(new_a, new_b, "square")
 
-    # compute SVD of A
+    # compute SVD of A & define C
     u, s, vt = np.linalg.svd(new_a)
-
     c = np.dot(np.dot(u.T, new_b), vt.T)
-    # create the intermediate array Y and the optimum symmetric transformation array X
+
+    # compute intermediate matrix Y
     n = new_a.shape[1]
     y = np.zeros((n, n))
     for i in range(n):
@@ -174,6 +174,7 @@ def symmetric(
             if s[i] ** 2 + s[j] ** 2 != 0:
                 y[i, j] = (s[i] * c[i, j] + s[j] * c[j, i]) / (s[i] ** 2 + s[j] ** 2)
 
+    # compute optimum symmetric transformation matrix X
     x = np.dot(np.dot(vt.T, y), vt)
     error = compute_error(new_a, new_b, x)
 
--- procrustes/test/test_symmetric.py
+++ procrustes/test/test_symmetric.py
@@ -76,11 +76,11 @@ def test_symmetric_scaled_shifted_transformed(m, n):
 def test_symmetric_with_small_values(m, n):
     r"""Test symmetric by arrays with small values."""
     # define an arbitrary array_a, translation matrix & symmetric matrix
-    array_a = np.random.uniform(0.0, 1e-6, (m, n))
+    array_a = np.random.uniform(0.0, 1.0e-6, (m, n))
     # Define shift to the rows of the matrices. This repeats a random matrix of size n, m times.
-    shift = np.tile(np.random.uniform(-1.e-6, 1.e-6, (n,)), (m, 1))
+    shift = np.tile(np.random.uniform(-1.0e-6, 1.0e-6, (n,)), (m, 1))
     # Random symmetric matrix.
-    rand_array = np.random.uniform(-1., 1., (n, n))
+    rand_array = np.random.uniform(-1.0, 1.0, (n, n))
     sym_array = (rand_array + rand_array.T) / 2.0
     # define array_b by scaling, translating and transforming array_a
     array_b = 6.61e-4 * array_a + shift
@@ -95,62 +95,62 @@ def test_symmetric_with_small_values(m, n):
 
 def test_not_full_rank_case():
     r"""Test symmetric with not full rank case."""
-    # Define a random matrix and symmetric matrix
+    # define a random matrix and symmetric matrix
     array_a = np.array([[10, 83], [52, 58], [58, 44]])
     sym_array = np.array([[0.38895636, 0.30523869], [0.30523869, 0.30856369]])
     array_b = np.dot(array_a, sym_array)
-    # compute procrustes transformation
+    # compute procrustes transformation & check results
     res = symmetric(array_a, array_b)
-    # check transformation is symmetric & error is zero
     assert_equal(res.s, None)
     assert_almost_equal(res.t, res.t.T, decimal=6)
     assert_almost_equal(res.error, 0.0, decimal=6)
 
 
 @pytest.mark.parametrize("m", np.random.randint(50, 100, (25,)))
+# @pytest.mark.parametrize("m, n", np.random.randint(50, 100, (25, 2)))
 def test_having_zero_eigenvalues_case(m):
     r"""Test symmetric that has zero singular values."""
+    # define a singular matrix (i.e. some eigenvalues are hard zeros)
     numb_nonzero_eigs = int(np.random.randint(1, m - 1))
-    # Define a matrix with not full eigenvalues values, e.g. 5 and 0 are eigenvalues values.
     sing_mat = list(np.random.uniform(-10.0, 10.0, (numb_nonzero_eigs,)))
     sing_mat += [0.] * (m - numb_nonzero_eigs)
     sing_mat = np.diag(sing_mat)
     array_a = ortho_group.rvs(m).dot(sing_mat).dot(ortho_group.rvs(m))
-    # Random symmetric matrix.
+    # generate random symmetric matrix & define matrix b
     rand_array = np.random.uniform(-1., 1., (m, m))
     sym_array = (rand_array + rand_array.T) / 2.0
-    # Defined array_b
     array_b = np.dot(array_a, sym_array)
-    # compute procrustes transformation
+    # compute procrustes transformation & check results
     res = symmetric(array_a, array_b)
-    # check transformation is symmetric & error is zero
     assert_equal(res.s, None)
     assert_almost_equal(res.t, res.t.T, decimal=6)
     assert_almost_equal(res.error, 0.0, decimal=6)
 
 
-@pytest.mark.parametrize("ncol", [2, 10, 15])
+@pytest.mark.parametrize("ncol", np.random.randint(2, 15, (3,)))
 def test_random_tall_rectangular_matrices(ncol):
     r"""Test Symmetric Procrustes with random tall matrices."""
+    # generate random floats in [0.0, 1.0) interval
     nrow = np.random.randint(ncol, ncol + 10)
     array_a, array_b = np.random.random((nrow, ncol)), np.random.random((nrow, ncol))
-
+    # minimize objective function to find transformation matrix
     desired, desired_func = minimize_one_transformation(array_a, array_b, ncol)
+    # compute transformation & check results
     res = symmetric(array_a, array_b, unpad_col=True, unpad_row=True)
+    assert_equal(res.s, None)
+    assert_almost_equal(np.abs(res.error - desired_func), 0.0, decimal=5)
+    assert_almost_equal(np.abs(res.t - desired), 0.0, decimal=3)
 
-    assert np.abs(res.error - desired_func) < 1e-5
-    assert np.all(np.abs(res.t - desired) < 1e-3)
 
-
-@pytest.mark.parametrize("nrow", [2, 10, 15])
+@pytest.mark.parametrize("nrow", np.random.randint(2, 15, (3,)))
 def test_fat_rectangular_matrices_with_square_padding(nrow):
     r"""Test Symmetric Procrustes with random wide matrices."""
-    # generate Random Rectangular Matrices
+    # generate random rectangular matrices
     ncol = np.random.randint(nrow + 1, nrow + 10)
     array_a, array_b = np.random.random((nrow, ncol)), np.random.random((nrow, ncol))
-
+    # minimize objective function to find transformation matrix
     _, desired_func = minimize_one_transformation(array_a, array_b, ncol)
     res = symmetric(array_a, array_b, pad=True)
-
-    # No uniqueness in solution, thus check that the optimal values are the same.
-    assert np.abs(res["error"] - desired_func) < 1e-5
+    # check results (solution is not uniqueness)
+    assert_almost_equal(np.abs(res.error - desired_func), 0.0, decimal=5)
+    assert_equal(res.s, None)

08b68e788b59e59fc08f64978e8cc446fde2fa62
Fanwang Meng <fwmeng88@gmail.com>
2022-08-22 13:40:37 -0400

    Remove unused imports

--- procrustes/test/test_generic.py
+++ procrustes/test/test_generic.py
@@ -23,7 +23,7 @@
 """Test procrustes.generic module."""
 
 import numpy as np
-from numpy.testing import assert_almost_equal, assert_raises
+from numpy.testing import assert_almost_equal
 from procrustes.generic import generic
 import pytest
 

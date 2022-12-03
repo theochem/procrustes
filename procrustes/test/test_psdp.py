25b43870a1c80ca9cd750554a7959b90f637675d
Abhishek Mittal <abhishekmittal964@gmail.com>
2022-12-03 17:20:38 +0530

    Fixed a minor linting error

--- procrustes/test/test_psdp.py
+++ procrustes/test/test_psdp.py
@@ -27,7 +27,6 @@ from numpy.testing import assert_almost_equal, assert_array_less
 from procrustes.psdp import psdp_opt, psdp_peng, psdp_projgrad, psdp_woodgate
 
 
-
 def test_psdp_projgrad_identity(n=np.random.randint(50, 100)):
     r"""Test Projected Gradient's algorithm for PSDP with identity matrix."""
     a = np.eye(n)
